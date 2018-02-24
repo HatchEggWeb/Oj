fda766eb5b0a8a70031b1364bde3790d39b24fa0
dogezhang <zhangyz86@hotmail.com>
2018-02-24 22:53:45 +0800

    添加contest-add

---
+++ Oj Admin/js/admin-contest-add.js
@@ -0,0 +1,12 @@
+var contestSecret = $('#contest-secret');
+var contestPassword = $('#contest-password');
+$('#contest-secret').change(function(event) {
+	//alert('');
+	if(contestSecret.prop('checked')){
+		//alert('ss');
+		document.getElementById('contest-password').disabled = false;
+	} else {
+		contestPassword.val('');
+		document.getElementById('contest-password').disabled = true;
+	}
+});
